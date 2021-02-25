1fdf76be1c24e26977ea2c58cfec1512056751fa
Nikita <pafflov2005@gmail.com>
2021-02-25 15:02:48 +0200

    Oleg molodec

---
+++ Metod/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Forecast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Author author = new Author("Petya", true);
+            Message message = new Message(author.username);
+            message.sendTime = DateTime.Now;
+            message.content = "SuperSecret text";
+            message.isReply = true;
+            message.WriteMessageToConsole();
+            author.GetAuthorInfo();
+        }
+    }
+
+    class Message
+    {
+        public string username;
+        public DateTime sendTime;
+        public string content;
+        public bool isRead = false;
+        public bool isReply;
+        public Message(string username)
+        {
+            this.username = username;
+        }
+        public void WriteMessageToConsole()
+        {
+            Console.WriteLine($"@{username}: {content} | {sendTime} | is read:{isRead}| is reply: {isReply}");
+        }
+    }
+
+    class Author
+    {
+        public string username;
+        public bool status;
+        public Author(string username, bool status)
+        {
+            this.username = username;
+            this.status = status;
+        }
+        public void GetAuthorInfo()
+        {
+            Console.WriteLine($"Username: {username} | Is online: {status}");
+        }
+
+
+    }
+
+}
---
+++ Reference/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Reference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine($"a = {a}");
+
+            Increment(a);
+            Console.WriteLine($"a передача по значению = {a}");
+            IncrementRef(ref a);
+            Console.WriteLine($"a передача по ссылке = {a}");
+        }
+        static void Increment(int b)
+        {
+            b++;  
+        }
+        static void IncrementRef(ref int b)
+        {
+            b++;
+        }
+    }
+}
